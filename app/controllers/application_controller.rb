d348fa88fe60efdfbd0334a857cfb5d9273fa86c
Jessica Nelson <yahwehschica@yahoo.com>
2022-05-17 11:26:09 -0400

    commit tree 59fad08fcca79af36ae41643cb7aad8dff01ed0b

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,23 @@
 class ApplicationController < Sinatra::Base
 
-  # add routes
+  set :default_content_type, 'application/json'
 
+  get "/bakeries" do
+   Bakery.all.to_json
+  end
+
+  get "/bakeries/:id" do
+    bakery = Bakery.find(params[:id])
+    bakery.to_json(include: :baked_goods)   
+  end
+
+  get "/baked_goods/by_price" do
+   baked_goods = BakedGood.by_price  
+   baked_goods.to_json 
+  end
+  
+  get "/baked_goods/most_expensive" do 
+    most_expensive = BakedGood.by_price.first
+    most_expensive.to_json
+  end
 end
--- app/models/baked_good.rb
+++ app/models/baked_good.rb
@@ -1,3 +1,7 @@
 class BakedGood < ActiveRecord::Base
-  # add association macro here
+  belongs_to :bakery
+
+  def self.by_price
+    self.all.order(price: :desc)
+  end
 end
